e6fda408f8ba68ed1686a06080a7bbd4cc8e70b5
Zebo Alieva <zeboalieva@air-zebo.mynetworksettings.com>
2023-12-26 17:00:42 -0500

    update v2

--- main.tf
+++ main.tf
@@ -1,67 +1,142 @@
-# provider "aws" {
- # access_key = "MY_AWS_ACCESS_KEY_ID"
- # secret_key = "MY_AWS_SECRET_ACCESS_KEY_ID"
- # region = "us-west-2"
-# }
-
-resource "aws_iam_group" "sysadmin" {
- name = "sysadmin"
- path = "/"
- }
-
-resource "aws_iam_user" "sysadmin1" {
- name = "sysadmin1"
- path = "/users/"
+resource "aws_iam_group" "SysAdmin" {
+  name = "SysAdmin"
 }
-resource "aws_iam_group_membership" "sysadmin1_membership" {
-  name = aws_iam_user.sysadmin1.name
-  users = [aws_iam_user.sysadmin1.name]
-  group = aws_iam_group.sysadmin.name
+ 
+resource "aws_iam_user" "Sysadmin1" {
+  name = "Sysadmin1"
 }
-# resource "aws_iam_group" "dbadmin" {
-#  name = "dbadmin"
-#  path = "/"
-#  }
+ 
+resource "aws_iam_user" "Sysadmin2" {
+  name = "Sysadmin2"
+}
+ 
+resource "aws_iam_group_membership" "assignment1" {
+  name = "sysadmin-membership"
+ 
+  users = [
+    aws_iam_user.Sysadmin1.name,
+    aws_iam_user.Sysadmin2.name
+  ]
+ 
+  group = aws_iam_group.SysAdmin.name
+}
+ 
+resource "aws_iam_group_policy_attachment" "admin" {
+  group      = aws_iam_group.SysAdmin.name
+  policy_arn = "arn:aws:iam::aws:policy/job-function/SystemAdministrator"
+}
+ 
+resource "aws_iam_group" "DBAdmin" {
+  name = "DBAdmin"
+}
+ 
+resource "aws_iam_user" "dbadmin1" {
+  name = "dbadmin1"
+}
+ 
+resource "aws_iam_user" "dbadmin2" {
+  name = "dbadmin2"
+}
+ 
+resource "aws_iam_group_membership" "assignment2" {
+  name = "dbadmin-membership"
+ 
+  users = [
+    aws_iam_user.dbadmin1.name,
+    aws_iam_user.dbadmin2.name
+  ]
+ 
+  group = aws_iam_group.DBAdmin.name
+}
+ 
+resource "aws_iam_group_policy_attachment" "database" {
+  group      = aws_iam_group.DBAdmin.name
+  policy_arn = "arn:aws:iam::aws:policy/job-function/DatabaseAdministrator"
+}
+ 
+resource "aws_iam_group" "Monitor" {
+  name = "Monitor"
+}
+ 
+resource "aws_iam_user" "monitoruser1" {
+  name = "monitoruser1"
+}
+ 
+resource "aws_iam_user" "monitoruser2" {
+  name = "monitoruser2"
+}
+ 
+resource "aws_iam_user" "monitoruser3" {
+  name = "monitoruser3"
+}
+ 
+resource "aws_iam_user" "monitoruser4" {
+  name = "monitoruser4"
+}
+ 
+resource "aws_iam_group_membership" "assignment3" {
+  name = "monitor-membership"
+ 
+  users = [
+    aws_iam_user.monitoruser1.name,
+    aws_iam_user.monitoruser2.name,
+    aws_iam_user.monitoruser3.name,
+    aws_iam_user.monitoruser4.name
+  ]
+ 
+  group = aws_iam_group.Monitor.name
+}
+ 
+resource "aws_iam_group_policy_attachment" "ec2" {
+  group      = aws_iam_group.Monitor.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+ 
+resource "aws_iam_group_policy_attachment" "s3" {
+  group      = aws_iam_group.Monitor.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+ 
+resource "aws_iam_group_policy_attachment" "RDS" {
+  group      = aws_iam_group.Monitor.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
 
-# resource "aws_iam_group" "monitor" {
-#  name = "monitor"
-#  path = "/"
-#  }
-
-# resource "aws_iam_user" "sysadmin2" {
-#  name = "sysadmin2"
-#  path = "/users/"
+resource "aws_iam_account_password_policy" "strict" {
+  minimum_password_length        = 8
+  max_password_age               = 90
+  password_reuse_prevention      = 3
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+ 
+}
+ 
+# resource "aws_iam_role" "EC2-role2" {
+#   name = "EC2-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Sid    = ""
+#         Principal = {
+#           Service = "ec2.amazonaws.com"
+#         }
+#       },
+#     ]
+#   })
 # }
-# resource "aws_iam_user" "dbadmin1" {
-#  name = "dbadmin1"
-#  path = "/users/"
-# }
-# resource "aws_iam_user" "dbadmin2" {
-#  name = "dbadmin2"
-#  path = "/users/"
-# }
-# resource "aws_iam_user" "monitoruser1" {
-#  name = "monitoruser1"
-#  path = "/users/"
-# }
-# resource "aws_iam_user" "monitoruser2" {
-#  name = "monitoruser2"
-#  path = "/users/"
-# }
-# resource "aws_iam_user" "monitoruser3" {
-#  name = "monitoruser3"
-#  path = "/users/"
-# }
-# resource "aws_iam_user" "monitoruser4" {
-#  name = "monitoruser4"
-#  path = "/users/"
-# }
-
-
-#module "security-groups" {
- # source  = "app.terraform.io/tf-class-september-20/security-groups/aws"
-  #version = "1.0.0"
-  #vpc_id          = aws_vpc.vpc.id
-  #security_groups = var.security_groups
-#}
-
+ 
+# resource "aws_iam_role_policy_attachment" "EC2-role" {
+#    role       = aws_iam_role.EC2-role2.name
+#    policy_arn = "arn:aws:iam::432898149300:role/EC2-role"
+# } 
+ 
+# resource "aws_iam_instance_profile" "bastion_profile" {
+#     name = "bastion_profile"
+#     role = aws_iam_role.EC2-role2.name
+#   }
--- variables.tf
+++ variables.tf
@@ -38,13 +38,37 @@ variable "security_groups" {
     }))
   }))
 }
-
-variable "ec2" {
-  type = map(object({
-    subnet_name = string,
-    # cidr_block = string
-    # availability_zone = string
-  }))
-  default = {
-  }
+variable "groups" {
+  type        = list(string)
+  default     = []
+  description = "List of group names for Terraform create, case create_groups variable be true"
 }
+
+variable "users" {
+  type        = map(any)
+  default     = {}
+  description = "Map for Terraform create users."
+
+}
+
+variable "create_groups" {
+  type        = bool
+  default     = false
+  description = "Define if Terraform will create new_groups based on variable groups ."
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Tags for all resources."
+}
+
+# variable "ec2" {
+#   type = map(object({
+#     subnet_name = string,
+#     # cidr_block = string
+#     # availability_zone = string
+#   }))
+#   default = {
+#   }
+# }
